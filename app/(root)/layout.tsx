767bd0e810185eb0d65a3cfa8254f614fb3e2904
Stephano Tsutsumi <63266761+Stephano-Tsutsumi@users.noreply.github.com>
2025-10-03 11:17:52 -0700

    Implement auth logic with better-auth and inngest worflows

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,25 @@
 import Header from "@/components/Header"
+import {getAuth} from "@/lib/better-auth/auth"
+import {headers} from "next/headers"
+import {redirect} from "next/navigation"
 
-const Layout = ({ children }: { children : React.ReactNode}) => {
+
+const Layout = async ({ children }: { children : React.ReactNode}) => {
+  const auth = await getAuth()
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  console.log('Session object:', JSON.stringify(session, null, 2))
+
+  if(!session?.user) redirect('/sign-in')
+  
+  const user = {
+    id: session.user.id,
+    name: session.user.name || 'User',
+    email: session.user.email
+  }
   return (
     <main className="min-h-screen text-gray-400">
-        <Header />
+        <Header user={user} />
         <div className="containe py-10">
             { children }
         </div>
